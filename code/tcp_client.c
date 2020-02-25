12544cd44a9f5bfe5f4225b6ae1e0d30c4d83e41
mattb20 <matt_burgoyne@live.co.uk>
2020-02-25 13:04:10 +0000

    define remote address

--- code/tcp_client.c
+++ code/tcp_client.c
@@ -8,5 +8,15 @@
 
 int main() {
 
-  return 0;
+  // create socket integer holds socket descriptor
+  int networkSocket;
+  networkSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+  // end point created
+  // specify an address for the socket
+  struct sockaddr_in serverAddress;
+  serverAddress.sin_family = AF_INET; // family set
+  // specify port
+  serverAddress.sin_port = htons(9002);
+  serverAddress.sin_addr.s_addr = INADDR_ANY;
 }
